8a799d24b2a4bc939464082a35090406039d6cf9
Linden Chiu <hchiu87@gmail.com>
2023-09-12 00:11:48 -0700

    communicate with katago

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,9 +10,10 @@ extern crate gtp;
 // use gtp::controller::Engine;
 // use gtp::Command;
 
+use std::io::Write;
 use std::process::{Command, Output};
-
-use std::time::Duration;
+//use tokio::process::Command;
+use std::{process::Stdio, time::Duration};
 use tauri::plugin::Builder;
 
 mod katago;
@@ -26,18 +27,44 @@ mod katago;
 //       println!("testing {:?}", ev);
 #[tokio::main]
 async fn main() {
-    async fn kata_go_call() -> Output {
-        Command::new("/usr/local/Cellar/katago/1.13.2/bin/katago")
-            .arg("gtp")
-            .arg("-model ")
-            .arg("/Users/lindentree/.katrain/kata1-b18c384nbt-s6857212928-d3491594038.bin.gz")
-            .arg("-config ")
-            .arg("/Users/lindentree/Desktop/gtp_example.cfg")
-            .output()
-            .expect("failed to execute process")
-    }
+    // async fn kata_go_call() -> Output {
+    //     let mut engine = Command::new("/usr/local/Cellar/katago/1.13.2/bin/katago")
+    //         .arg("gtp")
+    //         .arg("-model ")
+    //         .arg("/Users/lindentree/.katrain/kata1-b18c384nbt-s6857212928-d3491594038.bin.gz")
+    //         .arg("-config ")
+    //         .arg("/Users/lindentree/Desktop/gtp_example.cfg")
+    //         .output()
+    //         .expect("failed to execute process");
+    // }
 
-    println!("{:?}", kata_go_call().await);
+    let mut engine = Command::new("/usr/local/Cellar/katago/1.13.2/bin/katago")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .arg("gtp")
+        .arg("-model ")
+        .arg("/Users/lindentree/.katrain/kata1-b18c384nbt-s6857212928-d3491594038.bin.gz")
+        .arg("-config ")
+        .arg("/Users/lindentree/Desktop/gtp_example.cfg")
+        .spawn()
+        .expect("Failed to spawn child process");
+
+    println!("{:?}", engine);
+
+    let cmd = Command::new("name");
+
+    let mut child_stdin = engine.stdin.take().expect("Failed to open stdin");
+
+    child_stdin
+        .write_all(b"showboard\n")
+        .expect("Failed to write");
+
+    drop(child_stdin);
+
+    let output = engine.wait_with_output();
+
+    println!("output = {:?}", output.unwrap().stdout);
+
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
